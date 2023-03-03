0cb86e41de234940e6462f96c99bccab10f9ff43
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-03-04 01:56:31 +0530

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,72 +1,78 @@
 import random
 from blackjack_art import logo
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 print(logo)
 playGame = input("Do you want to play a game of BlackJack : Type 'y' or 'n': ")
 playerDeck = []
 computerDeck = []
-def results(playerDeck,computerDeck):
-	playerScore =  calculateScore(playerDeck)
-	computerScore = calculateScore(computerDeck)
+
+def results(playerScore,computerScore):
 	pdiff = 21 - playerScore
 	cdiff = 21 - computerScore
 	if(playerScore == computerScore):
 		return "It is a Tie"
-	elif(playerScore==21):
-		return "You have won"
+	elif(computerScore==21):
+		return "You lost, oppenent has the BlackJack"
+	elif(playerScore ==21):
+		return "You win, You have the BlackJack"
 	elif(playerScore > 21):
 		return "It's a bust"
-	elif(computerScore>21):
-		return "You lose"
+	elif(computerScore > 21):
+		return "The computer has a bust"
 	elif(pdiff < cdiff):
 		return "You win"
 	else:
 		return "You lose"
 
 def calculateScore(list):
+	finalScore = sum(list)
+	if 11 in list and  finalScore >21:
+		list.remove(11)
+		list.append(1)
+	finalScore = sum(list)
+	return finalScore
+
+def sum (list):
 	score = 0
 	for i in list:
 		score += i
 	return score
-
 def checkScore(list):
-	finalScore = 0
-	while(calculateScore(list)<=16):
+	finalScore = calculateScore(list)
+	while(finalScore<=16):
 		list.append(random.choice(cards))
 		finalScore =  calculateScore(list)
 	return finalScore
-	
+
+def inputInList(list,numberOfElements):
+	for i in range(0,numberOfElements):
+		list.append(random.choice(cards)) 
+	return list
+
 if(playGame == "y"):
-	pcard1 = random.choice(cards)
-	pcard2 = random.choice(cards)
-	ccard1 = random.choice(cards)
-	ccard2 = random.choice(cards)
-	playerDeck.append(pcard1)
-	playerDeck.append(pcard2)
-	computerDeck.append(ccard1)
-	computerDeck.append(ccard2)
+	playerDeck = inputInList(playerDeck,2)
+	computerDeck = inputInList(computerDeck,2)
 	playerScore = calculateScore(playerDeck) 
 	computerScore = calculateScore(computerDeck)
 	print(f"Your cards: {playerDeck}, current score: {playerScore}")
 	print(f"Computer's first card is {computerDeck[0]}")
-	cont = input("Do you want to continue ? 'y' or 'no' : ")
+	cont = input("Do you want to continue ? 'y' or 'n' : ")
 	if(cont == "y"):
-		pcard3 = random.choice(cards)
-		playerDeck.append(pcard3)
+		playerDeck = inputInList(playerDeck,1)
 		playerScore = calculateScore(playerDeck)
 		print(f"Your cards: {playerDeck}, current score: {playerScore}")
 		print(f"Computer's first card is {computerDeck[0]}")
 		print(f"Your final cards are: {playerDeck}, current score: {playerScore}")
-		computerScore = calculateScore(computerDeck)
-		print(f"Computer's final cards are: {computerDeck}, computer score = {computerScore}")
 		computerScore = checkScore(computerDeck)
+		print(f"Computer's final cards are: {computerDeck}, computer score = {computerScore}")
 			
 	else:
 		computerScore = checkScore(computerDeck)
 		print(f"Your final cards are: {playerDeck}, current score: {playerScore}")
 		computerScore = calculateScore(computerDeck)
 		print(f"Computer's final cards = {computerDeck}, computer score = {computerScore}")
-	print(results(playerDeck,computerDeck))
+	print(results(playerScore,computerScore))
 	
 else:
 	print("Have a Nice Day")
